3b4f639f1892ce37b37a103dc65da5f3c520495e
meleneth <meleneth@icehost>
2005-06-10 19:28:54 -0700

    connects and reads one line from server (localhost only)
    darcs-hash:20050611022854-c213b-0fc52d20da0b23b66cc7ef0a7d0623ba45924dfe.gz

--- src/blackbeard.cpp
+++ src/blackbeard.cpp
@@ -5,10 +5,16 @@ Console *console;
 
 int main(int argc, char *argv[])
 {
+    std::string servername = "localhost";
+    std::string groupname = "alt.binaries.moocows";
+
     console = new Console();
     console->print_logs = 1;
 
     TCPConnection *connection = new TCPConnection("localhost", 119);
+
     console->log("Connected");
+    console->log("Connecting to " + servername + " to grab article list for group " + groupname);
+
 
 }
