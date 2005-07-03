d71d84e7f45a3c0a9e34a00d9ed5ee4351f22da2
meleneth <meleneth@android>
2005-07-03 15:43:21 -0700

    its a good thing this code is never called
    darcs-hash:20050703224321-b89eb-e49b88642b1ed3ced52dfe3349baefb53de33292.gz

--- src/engine/uu_decoder.cpp
+++ src/engine/uu_decoder.cpp
@@ -27,6 +27,21 @@ void UUDecoder::open_file(void)
 
 void UUDecoder::decode_line(string line)
 {
+    size_t i;
+    for(i=0; i<line.size(); ++i){
+        line[i] -= ' ';
+    }
+
+    Uint32 num_encoded_chars = line[0];
+    size_t source = 1;
+    Uint32 manipulation;
+
+    manipulation  = line[source++] << 24;
+    manipulation |= line[source++] << 16;
+    manipulation |= line[source++] << 8;
+    manipulation |= line[source++];
+
+    string output_line(' ', 45);
 }
 
 // Private members go here.
