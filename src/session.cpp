36bfaf5aae34d6f63d7ba4b1416e8a37f6a2e4bd
zyz <zyz@zyz>
2005-07-16 21:02:24 -0700

    add Session : Screen object..  needs more work
    darcs-hash:20050717040224-7a690-2425016d7c73ce5ac870dd79ad383007f4ea2460.gz

---
+++ src/session.cpp
@@ -0,0 +1,25 @@
+#include "session.hpp"
+#include"globals.hpp"
+#include"postsetlistscreen.hpp"
+#include"postsetdetailscreen.hpp"
+#include"newsgrouplistscreen.hpp"
+
+Session::Session()
+{
+    widgets.push_front(new PostSetDetailScreen());
+//    widgets->push_front(new NewsGroupListScreen());
+    current_screen = new PostSetListScreen();
+    widgets.push_front(current_screen);
+}
+
+Session::~Session()
+{
+}
+
+void Session::render(void)
+{
+    if (current_screen){
+        current_screen->render();
+    }
+}
+
---
+++ src/session.hpp
@@ -0,0 +1,20 @@
+#ifndef ___session_inc
+#define ___session_inc 1
+
+#include"SDL_types.h"
+#include"screen.hpp"
+
+class Session : public Screen{
+    public:
+        Session();
+        ~Session();
+
+        virtual void render(void);
+
+        Widget *current_screen;
+        
+    private:
+    protected:
+};
+
+#endif
