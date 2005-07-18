8ce617bb1aeba926fd3bfaa3c14e955f7e286633
zyz <zyz@zyz>
2005-07-17 18:35:39 -0700

    hateses spaceses
    darcs-hash:20050718013539-7a690-727f46fa2357c4c655d8b991e42706d1e06f2022.gz

--- src/keydefs.hpp
+++ src/keydefs.hpp
@@ -1,13 +1,13 @@
 #ifndef ___keydefs_inc
 #define ___keydefs_inc 1
 
-#define IKEY_TAB         9 
+#define IKEY_TAB         9
 #define IKEY_ENTER       13
 #define IKEY_ESC         27
 #define IKEY_SLASH       47
 #define IKEY_Q           113
-#define IKEY_DOWNARROW   258  
-#define IKEY_UPARROW     259  
+#define IKEY_DOWNARROW   258
+#define IKEY_UPARROW     259
 #define IKEY_LEFTARROW   260
 #define IKEY_RIGHTARROW  261
 
