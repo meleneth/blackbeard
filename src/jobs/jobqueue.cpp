626ef05dbe1fd20cecabe6ac61a653eca25c4e8c
meleneth <meleneth@xerix>
2005-07-25 16:33:41 -0700

    reformat
    darcs-hash:20050725233341-2c5f9-02965371d1ae92b0464e43b67a94bcb788de6d09.gz

--- src/jobs/jobqueue.cpp
+++ src/jobs/jobqueue.cpp
@@ -23,11 +23,11 @@ Job *JobQueue::get_next_postset_job(void)
 {
     if(postset_jobs.size()){
         Job *job = *postset_jobs.begin();
-	PostsetJob* postsetjob = (PostsetJob*)job;
-	if (postsetjob->pieces_left_to_download() <= 0) {
-	    postset_jobs.pop_front();
-	    return NULL;
-	}
+        PostsetJob* postsetjob = (PostsetJob*)job;
+        if (postsetjob->pieces_left_to_download() <= 0) {
+            postset_jobs.pop_front();
+            return NULL;
+        }
         return postsetjob->get_next_job();
     }
     return NULL;
