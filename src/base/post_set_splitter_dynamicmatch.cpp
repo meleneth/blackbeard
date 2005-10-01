72af040d5ee9c9e207b058bac8645bf47587706d
zyz <zyz@zyz>
2005-10-01 01:02:43 -0700

    insane ramblings in the direction of a new post set splitter.  Needs "15 minutes of work and 2 functions"
    darcs-hash:20051001080243-7a690-8e8fc1a9d0015c8e36ec27a9809610d2c3ac9138.gz

--- src/base/post_set_splitter_dynamicmatch.cpp
+++ src/base/post_set_splitter_dynamicmatch.cpp
@@ -1,4 +1,5 @@
 #include "post_set_splitter_dynamicmatch.hpp"
+#include "console.hpp"
 
 PostSetSplitterDynamicMatch::PostSetSplitterDynamicMatch(NewsGroup *group):PostSetSplitter(group)
 {
@@ -11,5 +12,70 @@ PostSetSplitterDynamicMatch::~PostSetSplitterDynamicMatch()
 
 void PostSetSplitterDynamicMatch::process_header(MessageHeader *header)
 {
+    Uint32 max_no = active.size();
+    for(Uint32 i=0; i<max_no; ++i){
+        if (0 == active[i]->posted_by.compare(header->posted_by)){
+            if(active[i]->pattern->match(header->subject)){
+                active[i]->process_header(header);
+                return;
+            }
+        }
+    }
+    
+    max_no = unprocessed.size();
+    for(Uint32 i=0; i<max_no; ++i){
+        if(is_close(unprocessed[i], header)){
+            active.push_back(new PSDMSubMatch(header, unprocessed[i]));
+            reprocess_unprocessed();
+            return;
+        }
+    }
+
+    unprocessed.push_back(header);
 }
 
+Uint32 PostSetSplitterDynamicMatch::is_close(MessageHeader *h1, MessageHeader *h2)
+{
+    console->log("No TV and no Beer make Homer something something");
+    return 1;
+}
+
+void PostSetSplitterDynamicMatch::reprocess_unprocessed(void)
+{
+    Uint32 max_no = unprocessed.size();
+    Uint32 max_active_no = active.size();
+    for(Uint32 j=0; j<max_active_no; ++j){
+        for(Uint32 i=0; i<max_no; ++i){
+            if(active[j]->pattern->match(unprocessed[i]->subject)){
+                active[j]->process_header(unprocessed[i]);
+            }
+        }
+    }
+}
+
+string simple_x(string eatme)
+{
+    Uint32 max_no = eatme.length();
+    for(Uint32 i=0; i<max_no; ++i){
+        if((eatme[i] >= '0') && (eatme[i] <= '9')){
+            eatme[i] = 'X';
+        }
+    }
+
+    return eatme;
+
+}
+
+
+PSDMSubMatch::PSDMSubMatch(MessageHeader *h1, MessageHeader *h2)
+{
+    
+}
+
+PSDMSubMatch::~PSDMSubMatch()
+{
+}
+
+void PSDMSubMatch::process_header(MessageHeader *header)
+{
+}
--- src/base/post_set_splitter_dynamicmatch.hpp
+++ src/base/post_set_splitter_dynamicmatch.hpp
@@ -4,6 +4,24 @@
 #include"SDL_types.h"
 
 #include "post_set_splitter.hpp"
+#include "messageheader.hpp"
+#include "stringpattern.hpp"
+#include <vector>
+
+using std::vector;
+
+class PSDMSubMatch {
+    public:
+        PSDMSubMatch(MessageHeader *h1, MessageHeader *h2);
+        ~PSDMSubMatch();
+
+        void process_header(MessageHeader *header);
+        PostSet *postset;
+        StringPattern *pattern;
+        string posted_by;
+    private:
+    protected:
+};
 
 class PostSetSplitterDynamicMatch : public PostSetSplitter {
     public:
@@ -11,9 +29,14 @@ class PostSetSplitterDynamicMatch : public PostSetSplitter {
         ~PostSetSplitterDynamicMatch();
 
         virtual void process_header(MessageHeader *header);
+        Uint32 is_close(MessageHeader *h1, MessageHeader *h2);
+        void reprocess_unprocessed(void);
 
+        vector <MessageHeader *> unprocessed;
+        vector <PSDMSubMatch *> active;
     private:
     protected:
 };
+
 
 #endif
