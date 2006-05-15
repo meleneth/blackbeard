c0d5ac6724559c2402001b4372d38c185301bb2c
meleneth <meleneth@xerix>
2006-05-14 21:21:13 -0700

    add nzb lib
    darcs-hash:20060515042113-2c5f9-a7ac9dcacb39e5b565eafa7284f57824107f8885.gz

---
+++ src/nzb/mnzb.cpp
@@ -0,0 +1,14 @@
+#include "mnzb.hpp"
+
+mNZB::mNZB(int bar)
+{
+}
+
+mNZB::~mNZB()
+{
+}
+
+int mNZB::Foo(void)
+{
+}
+
---
+++ src/nzb/mnzb.hpp
@@ -0,0 +1,17 @@
+#ifndef ___mnzb_inc
+#define ___mnzb_inc 1
+
+#include "SDL_types.h"
+
+class mNZB {
+    public:
+        mNZB(int bar);
+        ~mNZB();
+
+        int Foo(void);
+
+    private:
+    protected:
+};
+
+#endif
